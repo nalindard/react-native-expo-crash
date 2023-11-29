023ff4f5069d5ed39f501f2c3f4351c01d28a930
Falcon <nalindard@gmail.com>
2023-11-29 08:17:09 +0530

    added material top

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,7 +7,8 @@ export default function TabLayout() {
       <Tabs.Screen name='index' options={{ headerTitle: 'Tab 01', tabBarLabel: 'Tabone', headerShown: !false }} />
       <Tabs.Screen name='second' options={{ headerTitle: 'Tab 02', tabBarLabel: 'Tabtwo', headerShown: !false }} />
       <Tabs.Screen name='third' options={{ headerTitle: 'Tab 03', tabBarLabel: 'Tabthree', headerShown: !false }} />
-      <Tabs.Screen name='fourth' options={{ headerTitle: 'Tab 04', tabBarLabel: 'Tabfour', headerShown: !false }} />
+      {/* <Tabs.Screen name='fourth' options={{ headerTitle: 'Tab 04', tabBarLabel: 'Tabfour', headerShown: !false,  }} /> */}
+      <Tabs.Screen name='materialtop' options={{ headerTitle: 'Material Top', tabBarLabel: 'materialtop', headerShown: !false }} />
     </Tabs>
   )
 }
---
+++ app/(tabs)/materialtop/settinngs.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Settinngs() {
+  return (
+    <View>
+      <Text>settinngs</Text>
+    </View>
+  )
+}
